ab4b5b5018cef393fed857450a93060e2e816820
Pavel Macenauer <pavel.macenauer@fotoaparat.cz>
2013-11-30 05:38:44 +0100

    Scene init done in host code and then its copied to CUDA where we work some magic

--- CudaRaytracer/scene.h
+++ CudaRaytracer/scene.h
@@ -1,121 +1,88 @@
 #ifndef SCENE_H
 #define SCENE_H
 
-#include "ray.h"
+#include <vector>
 #include "sphere.h"
 #include "plane.h"
-#include "color.h"
+#include "camera.h"
 
-namespace CUDA {
+class Scene
+{
+	public: 
+		uint32 getSphereCount() const { return spheres.size(); }
+		uint32 getPlaneCount() const { return planes.size(); }
 
-		struct Scene
-		{
-			Sphere* s;
+		void add(Sphere s) { spheres.push_back(s); }
+		void add(Plane p){ planes.push_back(p); }
+
+		Camera* getCamera() { return &camera; }
+		Sphere* getSpheres() { return &spheres[0]; }
+		Plane* getPlanes() { return &planes[0];	}
+
+		/*bool intersect(Ray const& ray) {
+			int i;
+			float st,pt;
+			st = 0.f;
+			pt = 0.f;
+			int maxPi,maxSi;
+			float tmp;
+			for (i=0;i<countS;i++){
+				tmp = s[i].intersect(ray);
+				if (st < tmp){
+					st = tmp;
+					maxSi =i;
+				}				
+			}
+			for (i=0;i<countP;i++){
+				tmp = p[i].intersect(ray);
+				if (pt < tmp){
+					pt = tmp;
+					maxPi = i;
+				}				
+			}
+			if ((pt==0.f) && (st == 0.f)){ //miss
+				t = 0.f;
+				//kind = skNone;
+				return false;
+			} else if (pt > st) //plane hit
+			{
+				point = ray.getPoint(pt);
+				normal = p[maxPi].normal;
+				t = pt;
+				interSColor = p[maxPi].color;
+
+	
+			}else if (st >= pt) //sphere hit
+			{
+				point = ray.getPoint(st);
+				normal = s[maxSi].getNormal(point);
+				t = st;
+				interSColor = s[maxSi].color;
+
+			};
+
+			return true;
+			    
+		}*/
+					
+	private:
+		std::vector<Sphere> spheres;
+		std::vector<Plane> planes;
+		Camera camera;
+
+				/*
+				Sphere* s;
 			int countS;
 			Plane* p;
 			int countP;
 			Color interSColor;
 			float t;
 			float3 point;
-			float3 normal;
+			float3 normal;*/
 			//enum ShapeKind { skPlane, skSphere, skNone } kind;
-
-		    __device__ Scene(){
-				countS = 0;
-				countP = 0;
-			}
-			__device__ bool add(Sphere &aSphere) {
-				if (countS == 0) {
-					countS++;
-					//cudaMalloc((Sphere *)s,sizeof(Sphere));
-				} else
-				{
-					countS++;
-					//s = (Sphere*) cudaRealloc(s,sizeof(Sphere)*countS);
-				}
-				if (s == NULL) {
-				  return false;				
-				} else
-				{
-				   s[countS-1] = aSphere;
-				   return true;
-				}
-				
-
-			
-			}
-
-			__device__ bool add(Plane &aPlane){
-				if (countP == 0) {
-					countP++;
-					//p = (Plane*) malloc(sizeof(Plane));
-				} else
-				{
-					countP++;
-					//p = (Plane*) realloc(p,sizeof(Plane)*countP);
-				}
-				if (p == NULL) {
-				  return false;				
-				} else
-				{
-				   p[countP-1] = aPlane;
-				   return true;
-				}
-			}
-			__device__ bool intersect(Ray const& ray) {
-		        int i;
-				float st,pt;
-				st = 0.f;
-				pt = 0.f;
-				int maxPi,maxSi;
-				float tmp;
-				for (i=0;i<countS;i++){
-					tmp = s[i].intersect(ray);
-					if (st < tmp){
-						st = tmp;
-						maxSi =i;
-					}				
-				}
-				for (i=0;i<countP;i++){
-					tmp = p[i].intersect(ray);
-					if (pt < tmp){
-						pt = tmp;
-						maxPi = i;
-					}				
-				}
-				if ((pt==0.f) && (st == 0.f)){ //miss
-					t = 0.f;
-					//kind = skNone;
-					return false;
-				} else if (pt > st) //plane hit
-				{
-					point = ray.getPoint(pt);
-					normal = p[maxPi].normal;
-					t = pt;
-					interSColor = p[maxPi].color;
-
-	
-				}else if (st >= pt) //sphere hit
-				{
-					point = ray.getPoint(st);
-					normal = s[maxSi].getNormal(point);
-					t = st;
-					interSColor = s[maxSi].color;
-
-				};
-
-				return true;
-			    
-			}
-			
-			__device__ ~Scene(){
-				//cudaFree(p);
-				//cudaFree(s);
-
-			}
 
 		};	
 
-}
+
 
 #endif
